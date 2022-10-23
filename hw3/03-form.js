6fa3b921811bec0dfb2f250e25ddbfb551c61aad
Drew Mahler <drewmahler@Drews-MBP-2.attlocal.net>
2022-10-23 15:07:54 -0700

    Implemented functionality for form submission. Unsure if my method of transfering the data from one page to the next was the right idea. Formatted with Prettier.

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,9 +1,41 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 5001;
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
 
 // http://localhost:5001/submit should return all the data the user entered
+let lastPost = "";
+
+const server = http.createServer((req, res) => {
+  req.on("data", (chunk) => {
+    lastPost += chunk;
+    console.log("on data: " + lastPost);
+  });
+
+  if (req.url === "/form") {
+    lastPost = "";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const formHTML = `
+    <form method="post">
+     Username: <input name="username"><br>
+     Email: <input name="email"><br>
+     <input type="submit">
+    </form>`;
+    res.write(formHTML);
+    res.end();
+  } else if (req.url === "/submit") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<p>");
+    lastPost.split("&").forEach((el) => {
+      console.log(el);
+      key = el.split("=")[0];
+      val = el.split("=")[1];
+      res.write(`${key}: ${val}<br>`);
+    });
+    res.write("<p>");
+    res.end();
+  }
+});
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
